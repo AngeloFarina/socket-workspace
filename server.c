7bd698d49e79338a92b7c0b231d4526125ce4b22
Angelo Farina <angi.farina@gmail.com>
2018-12-16 22:12:46 +0100

    added server.c

---
+++ server.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+
+#define LINE_LENGTH 128
+
+int main(int argc, char **argv){
+  
+}
